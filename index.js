814334cc8c11293f62beb234e244c055d78188ce
Keith Strickland <keith@redpillnow.com>
2018-02-13 16:42:26 -0500

    exported classes

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var behavior_1 = require("./models/behavior");
+var comment_1 = require("./models/comment");
+var component_1 = require("./models/component");
+var computed_1 = require("./models/computed");
+var function_1 = require("./models/function");
+var html_comment_1 = require("./models/html-comment");
+var listener_1 = require("./models/listener");
+var observer_1 = require("./models/observer");
+var property_1 = require("./models/property");
+module.exports.Behavior = behavior_1.Behavior;
+module.exports.Comment = comment_1.Comment;
+module.exports.Component = component_1.Component;
+module.exports.ComputedProperty = computed_1.ComputedProperty;
+module.exports.Function = function_1.Function;
+module.exports.HtmlComment = html_comment_1.HtmlComment;
+module.exports.Listener = listener_1.Listener;
+module.exports.Observer = observer_1.Observer;
+module.exports.Property = property_1.Property;
+
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import {Behavior} from './models/behavior';
+import {Comment} from './models/comment';
+import {Component} from './models/component';
+import {ComputedProperty} from './models/computed';
+import {Function} from './models/function';
+import {HtmlComment} from './models/html-comment';
+import {Listener} from './models/listener';
+import {Observer} from './models/observer';
+import {Property} from './models/property';
+
+module.exports.Behavior = Behavior;
+module.exports.Comment = Comment;
+module.exports.Component = Component;
+module.exports.ComputedProperty = ComputedProperty;
+module.exports.Function = Function;
+module.exports.HtmlComment = HtmlComment;
+module.exports.Listener = Listener;
+module.exports.Observer = Observer;
+module.exports.Property = Property;
